634c9ebf3a8daaf5edbe2d73a990fe19b96bb383
TheTensaii <25853572+thetensaii@users.noreply.github.com>
2023-01-22 01:12:42 -0500

    chore: setup storybook (#27)

---
+++ .storybook/main.js
@@ -0,0 +1,19 @@
+const path = require('path');
+module.exports = {
+  stories: [
+    "../components/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  staticDirs: ['../public'],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  framework: {
+    "name": "@storybook/nextjs",
+    "options": {}
+  },
+  docs: {
+    "autodocs": "tag"
+  }
+}
