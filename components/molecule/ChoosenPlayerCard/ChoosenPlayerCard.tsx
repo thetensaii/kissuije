b8c76deb2c3781212facf861d3677d547dfe5e74
TheTensaii <25853572+thetensaii@users.noreply.github.com>
2023-01-28 23:56:07 -0500

    feat: choosen player card component (#38)

---
+++ components/molecule/ChoosenPlayerCard/ChoosenPlayerCard.tsx
@@ -0,0 +1,16 @@
+import { Svg } from 'components/atom/Svg';
+import { PlayerType } from 'lib/frontend/types/player';
+import styles from './ChoosenPlayerCard.module.scss';
+
+type Props = {
+  player: PlayerType;
+};
+
+export const ChoosenPlayerCard = ({ player }: Props): JSX.Element => {
+  const { avatar, name } = player;
+  return (
+    <div className={styles.choosenPlayerCard}>
+      <Svg type={avatar} alt={avatar} width={32} height={32} /> {name}
+    </div>
+  );
+};
